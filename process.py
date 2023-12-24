b5cdefe9ddcb084313e2de6ccf628584aa0b2a3d
GiladLeef <96906027+GiladLeef@users.noreply.github.com>
2023-12-24 10:37:44 +0200

    added features

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import re
 
 def replace_syntax(input_code):
-    output_code = input_code.replace("scan", "scanf")
+    output_code = input_code.replace("int main()", "int main(int argc, char *argv[])")
     output_code = output_code.replace("File.write", "File::write")
     output_code = output_code.replace("File.read", "File::read")
     output_code = re.sub(r'string (\w+)', r'char \1[]', output_code)
@@ -16,24 +16,46 @@ def foreach_syntax(input_code):
     return output_code
 
 def print_syntax(input_code):
-    # Use regular expression to find print statements with variables
+    # Use regular expression to find print statements with variables or string literals
     pattern = re.compile(r'print\s*\(([^)]+)\)')
 
     # Find matches in the input code
-    match = pattern.search(input_code)
+    matches = pattern.finditer(input_code)
 
-    # If a match is found, replace the print statement
-    if match:
+    # Process each match in reverse order to avoid index issues
+    for match in reversed(list(matches)):
         print_statement = match.group(1)
-        
+
         # Replace '+' with ' << ' inside the print statement
         replaced_statement = re.sub(r'\s*\+\s*', ' << ', print_statement)
 
         # Construct the std::cout line
-        cout_line = f'std::cout << {replaced_statement} << std::endl;'
+        cout_line = f'std::cout << {replaced_statement} << std::endl'
 
         # Replace the print statement with the std::cout line in the entire code
-        modified_code = input_code.replace(match.group(0), cout_line)
+        input_code = input_code[:match.start()] + cout_line + input_code[match.end():]
+
+    return input_code
+
+def input_syntax(input_code):
+    # Use regular expression to find input statements with or without variables
+    pattern = re.compile(r'input\s*\(\s*([^)]*)\s*\)')
+
+    # Find matches in the input code
+    match = pattern.search(input_code)
+
+    # If a match is found, replace the input statement
+    if match:
+        input_statement = match.group(1)
+
+        # Replace ',' with ' >> ' inside the input statement
+        replaced_statement = re.sub(r'\s*,\s*', ' >> ', input_statement)
+
+        # Construct the std::cin line with reversed order
+        cin_line = f'std::cin >> {replaced_statement}'
+
+        # Replace the input statement with the reversed std::cin line in the entire code
+        modified_code = input_code.replace(match.group(0), cin_line)
 
         return modified_code
     else:
