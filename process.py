fabc7e4157d5e3ae9fd73dc78c7d6b8e6bda8e01
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-12-25 18:06:58 +0200

    Update process.py

--- process.py
+++ process.py
@@ -21,7 +21,12 @@ def add_namespace_std(code_content):
         if re.match(r'^\s*#include', line):
             found_includes = True
 
-    if not found_includes:
+    if found_includes:
+        # Find the index of the last #include line
+        last_include_index = max([i for i, line in enumerate(new_lines) if re.match(r'^\s*#include', line)], default=-1)
+        # Insert 'using namespace std;' after the last #include line
+        new_lines.insert(last_include_index + 1, 'using namespace std;')
+    else:
         new_lines.insert(0, 'using namespace std;')
 
     return '\n'.join(new_lines)
