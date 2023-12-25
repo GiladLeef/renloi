59032999b7a75f1efa12b084c7467f49c1aae984
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-12-25 06:08:15 +0200

    Update stdfile.h

--- include/stdfile.h
+++ include/stdfile.h
@@ -4,6 +4,35 @@
 
 class File {
 public:
+    // Specialization for string content
+    static void write(const char *filename, const char *content) {
+        std::ofstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file " << filename << " for writing." << std::endl;
+            return;
+        }
+
+        // Write the content to the file
+        file << content;
+
+        // The file will be closed automatically when 'file' goes out of scope
+    }
+
+    // General template for other types
+    template <typename T>
+    static void write(const char *filename, const T &content) {
+        std::ofstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file " << filename << " for writing." << std::endl;
+            return;
+        }
+
+        // Convert content to string and write it to the file
+        file << content;
+
+        // The file will be closed automatically when 'file' goes out of scope
+    }
+
     static std::string read(const char *filename) {
         std::ifstream file(filename);
         if (!file.is_open()) {
@@ -17,19 +46,5 @@ public:
         std::string content = content_stream.str();
 
         return content;
-    }
-
-    template <typename T>
-    static void write(const char *filename, const T &content) {
-        std::ofstream file(filename);
-        if (!file.is_open()) {
-            std::cerr << "Error opening file " << filename << " for writing." << std::endl;
-            return;
-        }
-
-        // Write the content to the file
-        file << content;
-
-        // The file will be closed automatically when 'file' goes out of scope
     }
 };
