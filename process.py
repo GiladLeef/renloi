3c0114e4dda8b31867c1bcbc1581b504e5381add
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-12-25 17:43:56 +0200

    Update process.py

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ def replace_syntax(input_code):
     output_code = input_code.replace("int main()", "int main(int argc, char *argv[])")
     output_code = output_code.replace("File.write", "File::write")
     output_code = output_code.replace("File.read", "File::read")
+    output_code = output_code.replace("Math.", "Math::")
     output_code = re.sub(r'string (\w+)', r'char \1[]', output_code)
     output_code = re.sub(r'char (\w+)\[\] = read\(([^)]+)\);', r'char *\1 = read(\2);', output_code)
 
