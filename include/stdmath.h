066c75ccb9d1ed61082a358fdcbb26a1f299b7e1
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-12-25 17:38:54 +0200

    Update stdmath.h

--- include/stdmath.h
+++ include/stdmath.h
@@ -1,27 +1,29 @@
 #include <cmath>
+class Math {
+public:
+    static double sqrt(double x) {
+        return std::sqrt(x);
+    }
 
-double sqrt(double x) {
-    return std::sqrt(x);
-}
+    static int abs(int x) {
+        return std::abs(x);
+    }
 
-int abs(int x) {
-    return std::abs(x);
-}
+    static double pow(double base, double exponent) {
+        return std::pow(base, exponent);
+    }
 
-double pow(double base, double exponent) {
-    return std::pow(base, exponent);
-}
+    static double round(double x) {
+        return std::round(x);
+    }
 
-double round(double x) {
-    return std::round(x);
-}
+    static template <typename T>
+    T min(T a, T b) {
+        return std::min(a, b);
+    }
 
-template <typename T>
-T min(T a, T b) {
-    return std::min(a, b);
-}
-
-template <typename T>
-T max(T a, T b) {
-    return std::max(a, b);
-}
+    static template <typename T>
+    T max(T a, T b) {
+        return std::max(a, b);
+    }
+};
