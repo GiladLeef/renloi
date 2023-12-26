6727fdc8dfb19fb9ed4fe28b413ecee471b72475
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2023-12-26 09:08:44 +0200

    Update stdnet.h

--- include/stdnet.h
+++ include/stdnet.h
@@ -7,6 +7,19 @@ public:
     class Http {
     public:
         static std::string get(const std::string& url) {
+            return performHttpRequest(url, CURLOPT_HTTPGET);
+        }
+
+        static std::string post(const std::string& url, const std::string& data) {
+            return performHttpRequest(url, CURLOPT_POSTFIELDS, data.c_str(), CURLOPT_POST);
+        }
+
+        static std::string put(const std::string& url, const std::string& data) {
+            return performHttpRequest(url, CURLOPT_POSTFIELDS, data.c_str(), CURLOPT_CUSTOMREQUEST, "PUT");
+        }
+
+    private:
+        static std::string performHttpRequest(const std::string& url, ...) {
             CURL* curl;
             CURLcode res;
 
@@ -17,6 +30,15 @@ public:
                 // Set the URL
                 curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
+                // Set additional options based on variable arguments
+                va_list args;
+                va_start(args, url);
+                int option;
+                while ((option = va_arg(args, int)) != 0) {
+                    curl_easy_setopt(curl, option, va_arg(args, void*));
+                }
+                va_end(args);
+
                 // Set the callback function to write the received data into a string
                 curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
                 std::string response;
@@ -24,7 +46,7 @@ public:
                 // Set the pointer to the response string
                 curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-                // Perform the HTTP GET request
+                // Perform the HTTP request
                 res = curl_easy_perform(curl);
 
                 // Check for errors
@@ -41,7 +63,6 @@ public:
             return "";
         }
 
-    private:
         // Callback function to write the received data into a string
         static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response) {
             size_t totalSize = size * nmemb;
