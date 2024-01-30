b4198eb1f319033b76bb3ecc407f085cf0a87ef4
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-01-30 10:04:31 +0200

    Update math.h

--- include/math.h
+++ include/math.h
@@ -6,14 +6,6 @@
 using uint64 = uint64_t;
 using int64 = int64_t;
 
-int randint(int low, int high) {
-    std::random_device rd;  // Use random_device to seed the generator
-    std::mt19937 mt(rd());  // Mersenne Twister 19937 generator
-    std::uniform_int_distribution<int> dist(low, high);  // Uniform distribution between low and high
-
-    return dist(mt);
-}
-
 class Math {
 public:
     static double sqrt(double x) {
@@ -65,4 +57,14 @@ public:
     static double atan(double x) {
         return std::atan(x);
     }
+    class Random {
+    public:
+        static int randint(int low, int high) {
+            std::random_device rd;  // Use random_device to seed the generator
+            std::mt19937 mt(rd());  // Mersenne Twister 19937 generator
+            std::uniform_int_distribution<int> dist(low, high);  // Uniform distribution between low and high
+
+            return dist(mt);
+        }
+    };
 };
