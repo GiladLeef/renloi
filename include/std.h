52a85695d92d7ed732054d9ca4a46c86f55280b5
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-01-30 09:59:59 +0200

    Update std.h

--- include/std.h
+++ include/std.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using size = size_t;
 
@@ -8,7 +9,36 @@ void print(const Args&... args) {
     (std::cout << ... << args) << std::endl;
 }
 
+// Function to mimic the input function
 template<typename T>
 void input(T& variable) {
     std::cin >> variable;
+}
+
+// Function to mimic the range function
+template<typename T>
+std::vector<T> range(T start, T end, T step = 1) {
+    std::vector<T> result;
+    for (T i = start; i < end; i += step) {
+        result.push_back(i);
+    }
+    return result;
+}
+
+// Function to mimic the map function
+template<typename Function, typename... Args>
+auto map(Function func, const Args&... args) {
+    return std::vector<decltype(func(args...))>{func(args...)...};
+}
+
+// Function to mimic the filter function
+template<typename Predicate, typename Container>
+auto filter(Predicate pred, const Container& container) {
+    std::vector<typename Container::value_type> result;
+    for (const auto& element : container) {
+        if (pred(element)) {
+            result.push_back(element);
+        }
+    }
+    return result;
 }
