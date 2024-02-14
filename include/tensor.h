3b203b15f1368209a77979bc47b4e25ada06e9b6
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-02-14 17:32:42 +0200

    Update tensor.h

--- include/tensor.h
+++ include/tensor.h
@@ -8,32 +8,32 @@ template <typename T>
 class Tensor {
 private:
     std::vector<std::vector<T>> data;
-    size rows;
-    size cols;
+    size_t rows;
+    size_t cols;
 
 public:
     // Constructor
-    Tensor(size rows, size cols) : rows(rows), cols(cols), data(rows, std::vector<T>(cols)) {}
+    Tensor(size_t rows, size_t cols) : rows(rows), cols(cols), data(rows, std::vector<T>(cols)) {}
 
     // Access element at position (i, j)
-    T& at(size i, size j) {
+    T& at(size_t i, size_t j) {
         return data.at(i).at(j);
     }
 
     // Get number of rows
-    size getRows() const {
+    size_t getRows() const {
         return rows;
     }
 
     // Get number of columns
-    size getCols() const {
+    size_t getCols() const {
         return cols;
     }
 
     // Display the tensor
     void display() const {
-        for (size i = 0; i < rows; ++i) {
-            for (size j = 0; j < cols; ++j) {
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < cols; ++j) {
                 std::cout << data[i][j] << "\t";
             }
             std::cout << std::endl;
@@ -41,8 +41,8 @@ public:
     }
     // Initialize default tensor elements
     void init() {
-        for (size i = 0; i < rows; ++i) {
-            for (size j = 0; j < cols; ++j) {
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < cols; ++j) {
                 at(i, j) = i * cols + j;
             }
         }
