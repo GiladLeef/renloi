e96600f51eb876eebbf20a0d6eababb523ebdbe8
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-08-12 07:24:12 +0300

    camelCase

--- process.py
+++ process.py
@@ -1,55 +1,53 @@
 import re
 
-def replace_syntax(input_code):
-    # Regex to match text within quotes (strings) and comments
-    string_pattern = r'(\".*?\")'
-    comment_pattern = r'(\/\/.*?$|\/\*.*?\*\/)'
-    
-    # Temporary placeholders for strings and comments
+
+def replaceSyntax(inputCode):
+    stringPattern = '(\\".*?\\")'
+    commentPattern = "(\\/\\/.*?$|\\/\\*.*?\\*\\/)"
     placeholders = []
-    
-    def replace_in_code(match):
-        # Add non-matching parts to the result
+
+    def replaceInCode(match):
         return match.group(0)
-    
-    def add_placeholder(match):
-        # Replace matches with placeholders
+
+    def addPlaceholder(match):
         placeholders.append(match.group(0))
-        return f'__PLACEHOLDER_{len(placeholders) - 1}__'
-    
-    # Replace strings and comments with placeholders
-    code_with_placeholders = re.sub(string_pattern, add_placeholder, input_code, flags=re.DOTALL | re.MULTILINE)
-    code_with_placeholders = re.sub(comment_pattern, add_placeholder, code_with_placeholders, flags=re.DOTALL | re.MULTILINE)
-    
-    # Replace scope resolution operator
-    code_with_scope_replacement = code_with_placeholders.replace("::", ".")
-    
-    # Restore original strings and comments
-    def restore_placeholder(match):
+        return f"__PLACEHOLDER_{len(placeholders) - 1}__"
+
+    codeWithPlaceholders = re.sub(
+        stringPattern, addPlaceholder, inputCode, flags=re.DOTALL | re.MULTILINE
+    )
+    codeWithPlaceholders = re.sub(
+        commentPattern,
+        addPlaceholder,
+        codeWithPlaceholders,
+        flags=re.DOTALL | re.MULTILINE,
+    )
+    codeWithScopeReplacement = codeWithPlaceholders.replace("::", ".")
+
+    def restorePlaceholder(match):
         index = int(match.group(1))
         return placeholders[index]
-    
-    final_code = re.sub(r'__PLACEHOLDER_(\d+)__', restore_placeholder, code_with_scope_replacement)
-    
-    return final_code
-    
-def add_namespace_std(code_content):
-    lines = code_content.split('\n')
 
-    new_lines = []
-    found_includes = False
+    finalCode = re.sub(
+        "__PLACEHOLDER_(\\d+)__", restorePlaceholder, codeWithScopeReplacement
+    )
+    return finalCode
 
+
+def addNamespaceStd(codeContent):
+    lines = codeContent.split("\n")
+    newLines = []
+    foundIncludes = False
     for line in lines:
-        new_lines.append(line)
-        if re.match(r'^\s*#include', line):
-            found_includes = True
-
-    if found_includes:
-        # Find the index of the last #include line
-        last_include_index = max([i for i, line in enumerate(new_lines) if re.match(r'^\s*#include', line)], default=-1)
-        # Insert 'using namespace std;' after the last #include line
-        new_lines.insert(last_include_index + 1, '\nusing namespace std;')
+        newLines.append(line)
+        if re.match("^\\s*#include", line):
+            foundIncludes = True
+    if foundIncludes:
+        lastIncludeIndex = max(
+            [i for i, line in enumerate(newLines) if re.match("^\\s*#include", line)],
+            default=-1,
+        )
+        newLines.insert(lastIncludeIndex + 1, "\nusing namespace std;")
     else:
-        new_lines.insert(0, 'using namespace std;')
-
-    return '\n'.join(new_lines)
+        newLines.insert(0, "using namespace std;")
+    return "\n".join(newLines)
--- renloi.py
+++ renloi.py
@@ -4,67 +4,65 @@ import os
 import re
 import process
 
-def process_file(input_file, output_file, keep, run, debug):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        input_code = f.read()
 
-    output_code = process.add_namespace_std(input_code)
-    output_code = process.replace_syntax(output_code)
-
-    with open(output_file + ".cpp", 'w', encoding='utf-8') as f:
-        f.write(output_code)
-
-    compile_command = [
-        'g++',
-        '-static-libgcc',
-        '-static-libstdc++',
-        '-static',
-        output_file + ".cpp",
-        '-O3',
-        '-o',
-        os.path.splitext(output_file)[0]
+def processFile(inputFile, outputFile, keep, run, debug):
+    with open(inputFile, "r", encoding="utf-8") as f:
+        inputCode = f.read()
+    outputCode = process.addNamespaceStd(inputCode)
+    outputCode = process.replaceSyntax(outputCode)
+    with open(outputFile + ".cpp", "w", encoding="utf-8") as f:
+        f.write(outputCode)
+    compileCommand = [
+        "g++",
+        "-static-libgcc",
+        "-static-libstdc++",
+        "-static",
+        outputFile + ".cpp",
+        "-O3",
+        "-o",
+        os.path.splitext(outputFile)[0],
     ]
-    
-    if 'include <net.h>' in input_code:
-        compile_command.append('-lcurl')
-        
-    if 'include <bint.h>' in input_code:
-        compile_command.append('-lgmp')
-
-    if os.name == 'nt':
-        compile_command.append('-IC:\\renloi\\include')
-    else:  # Assume Linux or other Unix-like systems
-        compile_command.append('-I/renloi/include')
-    
-    # Run g++ command
-    if debug:
-        print(compile_command)
-        subprocess.run(compile_command)
+    if "include <net.h>" in inputCode:
+        compileCommand.append("-lcurl")
+    if "include <bint.h>" in inputCode:
+        compileCommand.append("-lgmp")
+    if os.name == "nt":
+        compileCommand.append("-IC:\\renloi\\include")
     else:
-        subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+        compileCommand.append("-I/renloi/include")
+    if debug:
+        print(compileCommand)
+        subprocess.run(compileCommand)
+    else:
+        subprocess.run(compileCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if run:
-        subprocess.run([os.path.splitext(output_file)[0]])
-
-    # Remove the source file if not specified to keep
+        subprocess.run([os.path.splitext(outputFile)[0]])
     if not keep:
-        os.remove(output_file + ".cpp")
+        os.remove(outputFile + ".cpp")
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Renloi Programming Language Compiler.')
-    parser.add_argument('input_file', help='Input file path')
-    parser.add_argument('-output_file', help='Output file path (default: input file name without extension)', default=None)
-    parser.add_argument('-run', action='store_true', help='Run the compiled code')
-    parser.add_argument('-keep', action='store_true', help='Keep the source file after compilation')
-    parser.add_argument('-debug', action='store_true', help='Show g++ compilation warnings/errors')
+    parser = argparse.ArgumentParser(
+        description="Renloi Programming Language Compiler."
+    )
+    parser.addArgument("input_file", help="Input file path")
+    parser.addArgument(
+        "-output_file",
+        help="Output file path (default: input file name without extension)",
+        default=None,
+    )
+    parser.addArgument("-run", action="store_true", help="Run the compiled code")
+    parser.addArgument(
+        "-keep", action="store_true", help="Keep the source file after compilation"
+    )
+    parser.addArgument(
+        "-debug", action="store_true", help="Show g++ compilation warnings/errors"
+    )
+    args = parser.parseArgs()
+    if args.outputFile is None:
+        args.outputFile = os.path.splitext(os.path.basename(args.inputFile))[0]
+    processFile(args.inputFile, args.outputFile, args.keep, args.run, args.debug)
 
-    args = parser.parse_args()
 
-    # Set default output file name if not provided
-    if args.output_file is None:
-        args.output_file = os.path.splitext(os.path.basename(args.input_file))[0]
-
-    process_file(args.input_file, args.output_file, args.keep, args.run, args.debug)
-
-if __name__ == '__main__':
+if Name == "__main__":
     main()
